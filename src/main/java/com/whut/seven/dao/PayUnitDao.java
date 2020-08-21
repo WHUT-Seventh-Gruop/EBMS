d4ac8fb6cb4eaecca73c0ba9d45d11d2c57235c6
Unknown <unknown@example.com>
2020-08-21 22:46:19 +0800

    添加PayUnitDao

---
+++ src/main/java/com/whut/seven/dao/PayUnitDao.java
@@ -0,0 +1,7 @@
+package com.whut.seven.dao;
+
+import com.whut.seven.entity.PayUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayUnitDao extends JpaRepository<PayUnit, String> {
+}
