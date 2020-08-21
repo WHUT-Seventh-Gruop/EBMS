3699781e17e952925fb10f9f100eab71bfc28908
roomτ <zheng.rt@foxmail.com>
2020-08-21 20:46:42 +0800

    添加注册接口

--- src/main/java/com/whut/seven/service/UserService.java
+++ src/main/java/com/whut/seven/service/UserService.java
@@ -17,4 +17,13 @@ public interface UserService {
      * @return 登录的用户信息
      */
     User login(String username, String password, Integer role);
+
+    /**
+     * 注册用户
+     * @param username 账号
+     * @param password 密码（使用MD5加密
+     * @param role 身份
+     * @return 注册的用户信息
+     */
+    User register(String username, String password, Integer role);
 }
