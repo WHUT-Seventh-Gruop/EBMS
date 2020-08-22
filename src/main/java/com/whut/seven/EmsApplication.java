7f5e7e9af642669ece935829425f4b64bde51f72
HaokaiSun <929884628@qq.com>
2020-08-22 08:16:28 +0800

    添加Output本机IP显示

--- src/main/java/com/whut/seven/EmsApplication.java
+++ src/main/java/com/whut/seven/EmsApplication.java
@@ -3,11 +3,21 @@ package com.whut.seven;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class EmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmsApplication.class, args);
+        try {
+            InetAddress ip4 = Inet4Address.getLocalHost();
+            System.out.println("当前服务器IP为"+ip4.getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
 }
